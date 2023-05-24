50d3c6584c83719c457492e4fefbb0d98b2c4c36
Katie George <katiegeo@amazon.com>
2023-05-24 11:24:51 -0700

    WIP: resize

--- src/app-layout/index.tsx
+++ src/app-layout/index.tsx
@@ -296,6 +296,8 @@ const OldAppLayout = React.forwardRef(
     const drawerSize =
       selectedDrawer?.id && drawerSizes[selectedDrawer?.id] ? drawerSizes[selectedDrawer?.id] : toolsWidth;
 
+    console.log(drawerSizes);
+
     const splitPanelPosition = splitPanelPreferences?.position || 'bottom';
     const [splitPanelReportedToggle, setSplitPanelReportedToggle] = useState<SplitPanelSideToggleProps>({
       displayed: false,
